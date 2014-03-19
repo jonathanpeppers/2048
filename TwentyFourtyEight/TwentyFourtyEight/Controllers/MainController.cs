80d629d230d7808d9c8ec2506b7e979c5b655949
Jonathan Peppers <jonathan.peppers@gmail.com>
2014-03-19 12:56:54 -0500

    Added Xamarin.iOS project, starts site

---
+++ TwentyFourtyEight/TwentyFourtyEight/Controllers/MainController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace TwentyFourtyEight
+{
+    public partial class MainController : UIViewController
+    {
+        public MainController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _webView.LoadError += (sender, e) =>
+            {
+                Console.WriteLine("LoadError: " + e.Error.LocalizedDescription);
+            };
+
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            var url = NSUrl.FromFilename("Html/index.html");
+            var request = new NSUrlRequest(url);
+
+            _webView.LoadRequest(request);
+        }
+
+        public override bool PrefersStatusBarHidden()
+        {
+            return true;
+        }
+
+        public override bool ShouldAutorotate()
+        {
+            return false;
+        }
+
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations()
+        {
+            return UIInterfaceOrientationMask.Portrait;
+        }
+    }
+}
